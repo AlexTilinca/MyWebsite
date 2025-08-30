6f86c97f7fd47de4d212be2e6afcb04d9bfd60e8
Alex Tilinca <tilinca.alexandru@gmail.com>
2025-08-30 15:05:29 +0300

    npm run build not working

--- app/page.tsx
+++ app/page.tsx
@@ -304,7 +304,7 @@ export default function ITServicesWebsite() {
       message: ''
     });
 
-    const handleInputChange = (e) => {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setFormData({
         ...formData,
         [e.target.name]: e.target.value
